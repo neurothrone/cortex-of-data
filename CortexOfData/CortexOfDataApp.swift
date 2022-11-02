3aa470f34fc525d5a08006bbf67184d3254d8a25
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-02 06:37:42 +0100

    Initial Commit

---
+++ CortexOfData/CortexOfDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortexOfDataApp.swift
+//  CortexOfData
+//
+//  Created by Zaid Neurothrone on 2022-11-02.
+//
+
+import SwiftUI
+
+@main
+struct CortexOfDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
